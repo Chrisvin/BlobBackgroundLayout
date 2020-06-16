e01fb5055639eeebf9e1c6bb1fdf0e07346c59a1
Jem <chrisvin1997@gmail.com>
2020-06-17 05:07:21 +0530

    Add PointUtil

---
+++ blobbackground/src/main/java/com/jem/blobbackground/util/PointUtil.kt
@@ -0,0 +1,27 @@
+package com.jem.blobbackground.util
+
+import android.graphics.PointF
+import kotlin.math.cos
+import kotlin.math.sin
+
+object PointUtil {
+
+    fun getPointOnCircle(radius: Float, angle: Float): PointF {
+        return PointF(
+            (radius * cos(angle)),
+            (radius * sin(angle))
+        )
+    }
+
+    fun getIntermediatePoint(start: PointF, end: PointF, percent: Float): PointF {
+        return PointF(
+            lerp(start.x, end.x, percent),
+            lerp(start.y, end.y, percent)
+        )
+    }
+
+    private fun lerp(a: Float, b: Float, t: Float): Float {
+        return a + t * (b - a)
+    }
+
+}
