ce505b12a9e78ea5f4adb59e2acc22df6531405b
Jem <chrisvin1997@gmail.com>
2020-06-21 00:32:16 +0530

    Add BaseBlobLayout interface that contains all public fns

---
+++ blobbackground/src/main/java/com/jem/blobbackground/base/BaseBlobLayout.kt
@@ -0,0 +1,57 @@
+package com.jem.blobbackground.base
+
+import com.jem.blobbackground.model.Blob
+
+internal interface BaseBlobLayout {
+
+    /**
+     * Get blob count
+     */
+    fun getBlobCount(): Int
+
+    /**
+     * Add blob with specified configuration
+     */
+    fun addBlob(vararg blobConfig: Blob.Configuration)
+
+    /**
+     * Remove all blobs
+     */
+    fun removeBlobs()
+
+    /**
+     * Remove blob at index
+     */
+    fun removeBlob(index: Int)
+
+    /**
+     * Recreate all blobs with new shapes
+     */
+    fun recreateBlobs()
+
+    /**
+     * Recreate blob at index with new shape
+     */
+    fun recreateBlob(index: Int)
+
+    /**
+     * Get all blobConfigurations
+     */
+    fun getBlobConfigurations() : Array<Blob.Configuration>
+
+    /**
+     * Get configuration for Blob at index
+     */
+    fun getBlobConfiguration(index: Int) : Blob.Configuration
+
+    /**
+     * Update all blobConfigurations
+     */
+    fun updateBlobConfigurations(blobConfigs: Array<Blob.Configuration>)
+
+    /**
+     * Update configuration for BLob at index
+     */
+    fun updateBlobConfiguration(blobConfig: Blob.Configuration, index: Int)
+
+}
